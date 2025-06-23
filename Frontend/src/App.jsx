90065162e171775cdc4cb41c8b0ea1cb218b8845
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-23 17:15:16 +0200

    added. started routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./assets/Pages/HomePage";
+import DestinationPage from "./assets/Pages/DestinationPage";
+import Compare from "./assets/Pages/Compare";
 
 function App() {
-
-
   return (
-    <>
-
-    </>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/destination/:id" element={<DestinationPage />} />
+    <Route path="/compare" element={<Compare />} />
+    <Route path="/*" element={<NotFound />} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/assets/Pages/NotFound.jsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return (
+        <h2>OOPSSS... Qualcosa è andato storto.</h2>
+    )
+}
