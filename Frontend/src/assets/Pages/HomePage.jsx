3647cf6dfbee2df5777ca547ab29e887286863d1
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-25 19:38:58 +0200

    added. select

--- Frontend/src/assets/Pages/HomePage.jsx
+++ Frontend/src/assets/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 export default function HomePage() {
     const [destinations, setDestinations] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
+    const [categoryFilter, setCategoryFilter] = useState("")
 
     async function fetchDestinations() {
         try {
@@ -25,35 +26,53 @@ export default function HomePage() {
         fetchDestinations()
     }, [])
 
-    
+
     const filteredDestinations = destinations.filter((d) => {
         return d.title.toLowerCase().includes(searchQuery.trim().toLowerCase())
     })
-    
+
+    const categoryDestinations = destinations.filter((d) => {
+        if(categoryFilter === ""){
+            return true
+        }else{
+            return d.category.toLowerCase().includes(categoryFilter.trim().toLowerCase())
+        }
+    })
+
 
     return (
         <div>
 
-            <NavBar searchQuery={searchQuery}  setSearchQuery={setSearchQuery}/>
+            <NavBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+            <select 
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+            >
+                <option value="">Tutte le categorie</option>
+                <option value="Città d'arte">Città d'arte</option>
+                <option value="Metropoli">Metropoli</option>
+                <option value="Avventura e natura">Avventura e natura</option>
+                <option value="Mare e relax">Mare e relax</option>
+            </select>
             <div className="container-dests">
-            {
-                filteredDestinations && filteredDestinations.map((d) => (
+                {
+                    filteredDestinations && filteredDestinations.map((d) => (
 
-                    <div  className="container-single-dest" key={d.id}>
-                        
-                        <h3 className="dest-title" >{d.title}</h3>
-                        <img  className="img-dest" src={d.img}alt={d.title} />
-                        <div className="info-wrapper">
-                            <span className="span-dest" >{d.category}</span>
+                        <div className="container-single-dest" key={d.id}>
+
+                            <h3 className="dest-title" >{d.title}</h3>
+                            <img className="img-dest" src={d.img} alt={d.title} />
+                            <div className="info-wrapper">
+                                <span className="span-dest" >{d.category}</span>
+                            </div>
                         </div>
-                    </div>
 
 
 
 
-))
-}
-</div>
+                    ))
+                }
+            </div>
         </div>
 
 
