f0855a4c04d0237b01fbd973b26e308cf8c5f0eb
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-27 13:29:11 +0200

    added. filters for title, categories and ordering

--- Frontend/src/assets/Pages/HomePage.jsx
+++ Frontend/src/assets/Pages/HomePage.jsx
@@ -5,6 +5,7 @@ export default function HomePage() {
     const [destinations, setDestinations] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
     const [categoryFilter, setCategoryFilter] = useState("")
+    const [order, setOrder] = useState("")
 
     async function fetchDestinations() {
         try {
@@ -27,33 +28,52 @@ export default function HomePage() {
     }, [])
 
 
-    const filteredDestinations = destinations.filter((d) => {
-        return d.title.toLowerCase().includes(searchQuery.trim().toLowerCase())
-    })
 
-    const categoryDestinations = destinations.filter((d) => {
-        if(categoryFilter === ""){
-            return true
-        }else{
-            return d.category.toLowerCase().includes(categoryFilter.trim().toLowerCase())
-        }
-    })
+    const filteredDestinations = destinations
+        .filter((d) => {
+            return d.title.toLowerCase().includes(searchQuery.trim().toLowerCase()) &&
+                (categoryFilter === "" || d.category.toLowerCase().includes(categoryFilter.trim().toLowerCase()))
+                }) 
+        .sort((a,b) => {
+            if(order === "Crescente"){
+                return a.title.localeCompare(b.title)
+            }else if(order === "Decrescente"){
+                return b.title.localeCompare(a.title)
+            }
+            return 0
+               
+        })
 
 
     return (
         <div>
 
             <NavBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-            <select 
-            value={categoryFilter}
-            onChange={(e) => setCategoryFilter(e.target.value)}
-            >
-                <option value="">Tutte le categorie</option>
-                <option value="Città d'arte">Città d'arte</option>
-                <option value="Metropoli">Metropoli</option>
-                <option value="Avventura e natura">Avventura e natura</option>
-                <option value="Mare e relax">Mare e relax</option>
-            </select>
+            <div className="select-wrapper">
+
+                <select
+                    className="select-home"
+                    value={categoryFilter}
+                    onChange={(e) => setCategoryFilter(e.target.value)}
+                >
+                    <option className="option-select" value="">Tutte le categorie</option>
+                    <option className="option-select" value="Città d'arte">Città d'arte</option>
+                    <option className="option-select" value="Metropoli">Metropoli</option>
+                    <option className="option-select" value="Avventura e natura">Avventura e natura</option>
+                    <option className="option-select" value="Mare e relax">Mare e relax</option>
+                </select>
+
+                <select
+                    className="select-home"
+                    value={order}
+                    onChange={(e) => setOrder(e.target.value)}
+                >
+                    <option className="option-select" value="">Non Ordinare</option>
+                    <option className="option-select" value="Crescente">Crescente</option>
+                    <option className="option-select" value="Decrescente">Decrescente</option>
+
+                </select>
+            </div>
             <div className="container-dests">
                 {
                     filteredDestinations && filteredDestinations.map((d) => (
