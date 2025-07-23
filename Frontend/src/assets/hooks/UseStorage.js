f7c47dbaa1ed970c5df60ecf77bff6b81d72073e
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-07-23 15:33:59 +0200

    remove. comment

--- Frontend/src/assets/hooks/UseStorage.js
+++ Frontend/src/assets/hooks/UseStorage.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 export default function UseStorage(key, initialValue) {
   const [value, setValue] = useState(() => {
-    const saved = localStorage.getItem(key); //legge dal localStorage
-    return saved ? JSON.parse(saved) : initialValue; //se esiste fa il parse, se no restituisce l'initialValue
+    const saved = localStorage.getItem(key); 
+    return saved ? JSON.parse(saved) : initialValue; 
   });
 
-//funzione che aggiorna sia lo state di React, sia il localStorage con il nuovo valore.
+
   function setStoredValue(newValue) {
     setValue(newValue);
     localStorage.setItem(key, JSON.stringify(newValue));
